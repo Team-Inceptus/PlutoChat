9669e513e660415735b02249747da4e6c15107e4
GamerCoder215 <>
2022-01-07 18:54:08 +0000

    replit updates - Add Template for help command

--- src/us/teaminceptus/plutochat/commands/Help.java
+++ src/us/teaminceptus/plutochat/commands/Help.java
@@ -2,10 +2,19 @@ package us.teaminceptus.plutochat.commands;
 
 import us.teaminceptus.plutochat.PlutoChat;
 
-public class Help {
+public class Help implements CommandExecutor {
+
+	protected PlutoChat plugin;
 
 	public Help(PlutoChat plugin) {
-		// TODO Auto-generated constructor stub
+		this.plugin = plugin;
+		plugin.getCommand("plutohelp").setExecutor(this);
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+ 		
+		return true;
 	}
 
 }
