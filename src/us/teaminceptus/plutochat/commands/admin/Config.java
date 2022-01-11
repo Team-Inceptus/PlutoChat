d998682abec632884b2730bbb0e58f6e38d3ebeb
GamerCoder215 <gamercoder215@gmail.com>
2022-01-10 18:23:58 -0600

    Update Config.java

--- src/us/teaminceptus/plutochat/commands/admin/Config.java
+++ src/us/teaminceptus/plutochat/commands/admin/Config.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import us.teaminceptus.plutochat.PlutoChat;
+import us.teaminceptus.plutochat.PlutoChat.PlutoError;
 
 public class Config implements TabExecutor {
 
@@ -17,8 +19,8 @@ public class Config implements TabExecutor {
 	
 	public Config(PlutoChat plugin){
 		this.plugin = plugin;
-		plugin.getCommand("configuration").setExecutor(this);
-		plugin.getCommand("configuration").setTabCompleter(this);
+		plugin.getCommand("plutoconfiguration").setExecutor(this);
+		plugin.getCommand("plutoconfiguration").setTabCompleter(this);
 	}
 
 	@Override
@@ -52,7 +54,7 @@ public class Config implements TabExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (args.length < 1) {
-			PlutoChat.sendError(sender, Error.ARGS);
+			PlutoChat.sendError(sender, PlutoError.ARGS);
 			return false;
 		}
 
@@ -61,14 +63,14 @@ public class Config implements TabExecutor {
 		switch (args[0].toLowerCase()) {
 			case "get": {
 				if (args.length < 2) {
-					PlutoChat.sendError(sender, Error.ARGS);
+					PlutoChat.sendError(sender, PlutoError.ARGS);
 					return false;
 				}
 				
-				Set<String> keys = config.getKeys(false).stream().filter(key -> !(config.isConfigurationSection(key))).toList();
+				List<String> keys = config.getKeys(false).stream().filter(key -> !(config.isConfigurationSection(key))).toList();
 
 				if (!(keys.contains(args[1]))) {
-					PlutoChat.sendError(sender, Error.ARGS_INVALID);
+					PlutoChat.sendError(sender, PlutoError.ARGS_INVALID);
 					return false;
 				}
 
@@ -78,24 +80,24 @@ public class Config implements TabExecutor {
 			}
 			case "set": {
 				if (args.length < 2) {
-					PlutoChat.sendError(sender, Error.ARGS);
+					PlutoChat.sendError(sender, PlutoError.ARGS);
 					return false;
 				}
 				
-				Set<String> keys = config.getKeys(false).stream().filter(key -> !(config.isConfigurationSection(key))).toList();
+				List<String> keys = config.getKeys(false).stream().filter(key -> !(config.isConfigurationSection(key))).toList();
 
 				if (!(keys.contains(args[1]))) {
-					PlutoChat.sendError(sender, Error.ARGS_INVALID);
+					PlutoChat.sendError(sender, PlutoError.ARGS_INVALID);
 					return false;
 				}
 
 				if (args.length < 3) {
-					PlutoChat.sendError(sender, Error.ARGS_VALUE);
+					PlutoChat.sendError(sender, PlutoError.ARGS_VALUE);
 					return false;
 				}
 
 				if (config.isBoolean(args[1]) && !(args[2].equalsIgnoreCase("true")) && !(args[2].equalsIgnoreCase("false"))) {
-					PlutoChat.sendError(sender, Error.ARGS_BOOLEAN);
+					PlutoChat.sendError(sender, PlutoError.ARGS_BOOLEAN);
 					return false;
 				}
 
@@ -104,11 +106,13 @@ public class Config implements TabExecutor {
 					else if (config.isInt(args[1])) config.set(args[1], Integer.parseInt(args[2]));
 					else if (config.isDouble(args[1])) config.set(args[1], Double.parseDouble(args[2]));
 					else config.set(args[1], args[2]);
+					
+					sender.sendMessage(ChatColor.GREEN + "Successfully set " + ChatColor.GOLD + args[1] + ChatColor.GREEN + " to \"" + ChatColor.RESET + ChatColor.translateAlternateColorCodes('&', args[2]) + ChatColor.GREEN + "\"");
 				} catch (NumberFormatException e) {
 					if (config.isInt(args[1])) {
-						PlutoChat.sendError(sender, Error.ARGS_INT);
+						PlutoChat.sendError(sender, PlutoError.ARGS_INT);
 					} else {
-						PlutoChat.sendError(sender, Error.ARGS);
+						PlutoChat.sendError(sender, PlutoError.ARGS);
 					}
 					return false;
 				}
@@ -120,6 +124,7 @@ public class Config implements TabExecutor {
 				return false;
 			}
 		}
+		plugin.saveConfig();
 		return true;
 	}
 
