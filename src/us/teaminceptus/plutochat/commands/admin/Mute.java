7168b6c8e6690a3c6992c2ec5ca2c7f417976b03
GamerCoder215 <gamercoder215@gmail.com>
2022-01-10 18:24:09 -0600

    Update Mute.java

--- src/us/teaminceptus/plutochat/commands/admin/Mute.java
+++ src/us/teaminceptus/plutochat/commands/admin/Mute.java
@@ -1,6 +1,13 @@
 package us.teaminceptus.plutochat.commands.admin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
 import us.teaminceptus.plutochat.PlutoChat;
+import us.teaminceptus.plutochat.PlutoChat.PlutoError;
+import us.teaminceptus.plutochat.utils.PlutoUtils;
 
 public class Mute implements CommandExecutor {
 
@@ -14,12 +21,12 @@ public class Mute implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (args.length < 1) {
-			PlutoChat.sendError(sender, Error.ARGS_PLAYER);
+			PlutoChat.sendError(sender, PlutoError.ARGS_PLAYER);
 			return false;
 		}
 
 		if (Bukkit.getOfflinePlayer(PlutoUtils.nameToUUID(args[0])) == null) {
-			PlutoChat.sendError(sender, Error.ARGS_PLAYER);
+			PlutoChat.sendError(sender, PlutoError.ARGS_PLAYER);
 			return false;
 		}
 
