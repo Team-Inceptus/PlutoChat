4498415e05a09b2ef125256f953802a6834d359d
GamerCoder215 <gamercoder215@gmail.com>
2022-01-16 18:48:44 -0600

    Update Config.java

--- src/us/teaminceptus/plutochat/commands/admin/Config.java
+++ src/us/teaminceptus/plutochat/commands/admin/Config.java
@@ -1,14 +1,17 @@
 package us.teaminceptus.plutochat.commands.admin;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 
 import us.teaminceptus.plutochat.PlutoChat;
 import us.teaminceptus.plutochat.PlutoChat.PlutoError;
@@ -31,10 +34,11 @@ public class Config implements TabExecutor {
 		
 		switch (args.length) {
 			case 1: {
-				suggestions.addAll(Arrays.asList("get", "set"));
+				suggestions.addAll(Arrays.asList("get", "set", "reload"));
 				break;
 			}
 			case 2: {
+				if (!(args[0].equalsIgnoreCase("reload")))
 				suggestions.addAll(config.getKeys(false).stream().filter(key -> !(config.isConfigurationSection(key))).toList());
 				break;
 			}
@@ -100,14 +104,26 @@ public class Config implements TabExecutor {
 					PlutoChat.sendError(sender, PlutoError.ARGS_BOOLEAN);
 					return false;
 				}
-
+				
+				List<String> valueArgs = new ArrayList<>();
+				for (int i = 2; i < args.length; i++) {
+					valueArgs.add(args[i]);
+				}
+				
+				String value = String.join(" ", valueArgs);
+				
 				try {
-					if (config.isBoolean(args[1])) config.set(args[1], Boolean.parseBoolean(args[2]));
-					else if (config.isInt(args[1])) config.set(args[1], Integer.parseInt(args[2]));
-					else if (config.isDouble(args[1])) config.set(args[1], Double.parseDouble(args[2]));
-					else config.set(args[1], args[2]);
+					if (config.isBoolean(args[1])) config.set(args[1], Boolean.parseBoolean(value));
+					else if (config.isInt(args[1])) config.set(args[1], Integer.parseInt(value));
+					else if (config.isDouble(args[1])) config.set(args[1], Double.parseDouble(value));
+					else config.set(args[1], value);
 					
-					sender.sendMessage(ChatColor.GREEN + "Successfully set " + ChatColor.GOLD + args[1] + ChatColor.GREEN + " to \"" + ChatColor.RESET + ChatColor.translateAlternateColorCodes('&', args[2]) + ChatColor.GREEN + "\"");
+					sender.sendMessage(ChatColor.GREEN + "Successfully set " + ChatColor.GOLD + args[1] + ChatColor.GREEN + " to \"" + ChatColor.RESET + ChatColor.translateAlternateColorCodes('&', value) + ChatColor.GREEN + "\"");
+					try {
+						PlutoChat.getPlayersConfig().save(PlutoChat.getPlayersFile());
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
 				} catch (NumberFormatException e) {
 					if (config.isInt(args[1])) {
 						PlutoChat.sendError(sender, PlutoError.ARGS_INT);
@@ -117,6 +133,32 @@ public class Config implements TabExecutor {
 					return false;
 				}
 				
+				PlutoChat.checkConfigs();
+				
+				for (Player p : Bukkit.getOnlinePlayers()) {
+					if (config.getBoolean("TopTabEnabled")) {
+						p.setPlayerListHeader("\n" + ChatColor.translateAlternateColorCodes('&', config.getString("TopTab")) + "\n");
+					} else p.setPlayerListHeader("");
+					
+					if (config.getBoolean("BottomTabEnabled")) {
+						p.setPlayerListFooter("\n" + ChatColor.translateAlternateColorCodes('&', config.getString("BottomTab")) + "\n");
+					} else p.setPlayerListFooter("");
+				}
+				
+				break;
+			}
+			case "reload": {
+				plugin.reloadConfig();
+				plugin.saveConfig();
+				PlutoChat.checkConfigs();
+				try {
+					PlutoChat.getPlayersConfig().save(PlutoChat.getPlayersFile());
+				} catch (IOException e) {
+					plugin.getLogger().info("Error reloading config");
+					e.printStackTrace();
+				}
+				
+				sender.sendMessage(ChatColor.GREEN + "Successfully Reloaded Configuration!");
 				break;
 			}
 			default: {
